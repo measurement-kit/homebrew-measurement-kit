f6ab86ec57bd5cd7b44b2cdf32e51d3319d170a6
Simone Basso <bassosimone@gmail.com>
2017-09-05 21:01:14 +0200

    Build MK v0.7.1

--- measurement-kit.rb
+++ measurement-kit.rb
@@ -1,9 +1,9 @@
 class MeasurementKit < Formula
   desc "Portable C++11 network measurement library"
   homepage "https://measurement-kit.github.io/"
-  version "v0.7.0"
-  url "https://github.com/measurement-kit/measurement-kit/releases/download/v0.7.0/measurement-kit-0.7.0.tar.gz"
-  sha256 "57344bccdae40d8a4fb2f990653dcfb778e68d7381eba38c34f6e82a6beeca44"
+  version "v0.7.1"
+  url "https://github.com/measurement-kit/measurement-kit/releases/download/v0.7.1/measurement-kit-0.7.1.tar.gz"
+  sha256 "0d6ede8c2f978ecfd591774e75b53186743e32683039404364ae845646d48d2a"
 
   depends_on "libevent"
   depends_on "geoip"
