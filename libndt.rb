2951166b5052d126a13b51a28443d2f23e11d699
Simone Basso <bassosimone@gmail.com>
2018-07-01 17:53:12 +0200

    libndt: build shared library

--- libndt.rb
+++ libndt.rb
@@ -1,14 +1,15 @@
 class Libndt < Formula
   desc "NDT C++11 client library"
   homepage "https://measurement-kit.github.io/"
-  version "v0.23.0"
+  version "v0.23.0-1"
   url "https://github.com/measurement-kit/libndt/archive/v0.23.0.tar.gz"
   sha256 "66ece68f0c79b3cd96b4e27ef9a2ae8815c475bf3b2562a8e295566dc2464df1"
 
   depends_on "cmake" => :build
 
   def install
-    system "cmake", "-DCMAKE_BUILD_TYPE=Release", "-DCMAKE_INSTALL_PREFIX=#{prefix}", "."
+    system "cmake", "-DCMAKE_BUILD_TYPE=Release", "-DBUILD_SHARED_LIBS=ON", \
+        "-DCMAKE_INSTALL_PREFIX=#{prefix}", "."
     system "cmake", "--build", "."
     system "./tests"
     system "make", "install"
