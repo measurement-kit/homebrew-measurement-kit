6ad8991afc27f4493727711af627ca9837ce28a0
Simone Basso <bassosimone@gmail.com>
2018-07-03 19:03:07 +0200

    Build libndt v0.24.0

--- libndt.rb
+++ libndt.rb
@@ -1,9 +1,9 @@
 class Libndt < Formula
   desc "NDT C++11 client library"
   homepage "https://measurement-kit.github.io/"
-  version "v0.23.0-1"
-  url "https://github.com/measurement-kit/libndt/archive/v0.23.0.tar.gz"
-  sha256 "66ece68f0c79b3cd96b4e27ef9a2ae8815c475bf3b2562a8e295566dc2464df1"
+  version "v0.24.0"
+  url "https://github.com/measurement-kit/libndt/archive/v0.24.0.tar.gz"
+  sha256 "6b973b838764579614695994b8f90f588ca191e2dd42efc1165193eccbafa396"
 
   depends_on "cmake" => :build
 
