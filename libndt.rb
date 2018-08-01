787efaeab901990705e826dd7a5862edccb68ddd
Simone Basso <bassosimone@gmail.com>
2018-08-01 11:00:02 +0200

    Upgrade to libndt v0.26.0

--- libndt.rb
+++ libndt.rb
@@ -1,17 +1,18 @@
 class Libndt < Formula
   desc "NDT C++11 client library"
   homepage "https://measurement-kit.github.io/"
-  version "v0.25.1"
-  url "https://github.com/measurement-kit/libndt/archive/v0.25.1.tar.gz"
-  sha256 "42fff0e108ea816fb76218e923c8b44a02eb478b115fd7e460d7f76880b783e2"
+  version "v0.26.0"
+  url "https://github.com/measurement-kit/libndt/archive/v0.26.0.tar.gz"
+  sha256 "84877c9f5b4e4bcd9f13a390709f894778ccc9cb4fc370e3696dd96873021b6a"
 
   depends_on "cmake" => :build
 
   def install
-    system "cmake", "-DCMAKE_BUILD_TYPE=Release", "-DBUILD_SHARED_LIBS=ON", \
+    system "cmake", "-DCMAKE_BUILD_TYPE=Release", \
         "-DCMAKE_INSTALL_PREFIX=#{prefix}", "."
     system "cmake", "--build", "."
-    system "./tests"
+    system "./tests-curl"
+    system "./tests-libndt"
     system "make", "install"
   end
 end
