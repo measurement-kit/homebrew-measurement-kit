4ceff3f64d40e292d3c4d1851e5f4233c941ed87
Simone Basso <bassosimone@gmail.com>
2019-05-29 12:55:45 +0200

    macos: generate MK package with correct name
    Closes #16

--- measurement-kit.rb
+++ measurement-kit.rb
@@ -1,13 +1,13 @@
 class MeasurementKit < Formula
   desc "Portable C++11 network measurement library"
   homepage "https://measurement-kit.github.io/"
-  version "v0.10.4"
+  version "0.10.4"
   url "https://github.com/measurement-kit/measurement-kit/archive/v0.10.4.tar.gz"
   sha256 "6ca0d9e7a9c1ff0ea8713bf59fde9f87365acdc4b784a5a4bb3f35a77bc4b775"
 
   bottle do
     root_url "https://dl.bintray.com/measurement-kit/homebrew"
-    sha256 "afc26befc6aa2ebae8e8cf3dfe64f186e738e0336e1b67a2765757f6840435be" => :mojave
+    sha256 "cbf8608a90d18e2ee35dabe5775428ac38d013b7c14e6c8c095ab373d728b7ef" => :mojave
   end
 
   depends_on "libevent"
