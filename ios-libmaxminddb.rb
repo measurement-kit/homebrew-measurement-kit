7e77ee8f50a10b948f5fc589116d534723cd3ea6
Simone Basso <bassosimone@gmail.com>
2019-05-29 17:03:42 +0200

    ios: libmaxminddb: rebuild 1.3.2 with better formula
    Take into account that each arch needs to build into a separate
    directory, because that is significantly more clean.

--- ios-libmaxminddb.rb
+++ ios-libmaxminddb.rb
@@ -3,10 +3,11 @@ class IosLibmaxminddb < Formula
   homepage "https://github.com/maxmind/libmaxminddb"
   url "https://github.com/maxmind/libmaxminddb/releases/download/1.3.2/libmaxminddb-1.3.2.tar.gz"
   sha256 "e6f881aa6bd8cfa154a44d965450620df1f714c6dc9dd9971ad98f6e04f6c0f0"
+  revision 1
 
   bottle do
     root_url "https://dl.bintray.com/measurement-kit/homebrew"
-    sha256 "c8f9c66df0e0f4d2d38a9e2bc0ce37c969e23608fc44b707a709f9e357a2e60d" => :mojave
+    sha256 "08b292b748c4bb70eb4a3b845e2a9fa984733f0f30c0b95bee78d6ca1d1a8742" => :mojave
   end
 
   depends_on "cross" => :build
@@ -14,14 +15,15 @@ class IosLibmaxminddb < Formula
   keg_only "iOS build that we should not install system wide"
 
   def install
-    for arch in [ "i386", "x86_64", "armv7s", "arm64" ] do
-      system "cross-configure-ios", "#{arch}", "--disable-debug",
+    [ "i386", "x86_64", "armv7s", "arm64" ].each do |arch|
+      mkdir "build-#{arch}" do
+        system "cross-ios", "#{arch}", "../configure", "--disable-debug",
                             "--disable-dependency-tracking",
                             "--disable-shared",
                             "--disable-tests",
                             "--prefix=#{prefix}/#{arch}"
-      system "make", "install"
-      system "make", "distclean"
+        system "make", "install"
+      end
     end
   end
 end
