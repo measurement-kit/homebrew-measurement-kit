7750a5abad101415a225e5fc8dca8c4affd0ad50
Simone Basso <bassosimone@gmail.com>
2019-06-03 12:36:42 +0200

    Update to measurement-kit/cross@0.4.1

--- cross.rb
+++ cross.rb
@@ -1,9 +1,9 @@
 class Cross < Formula
   desc "Cross build scripts for MK"
   homepage "https://measurement-kit.github.io/"
-  version "0.4.0"
-  url "https://github.com/measurement-kit/cross/archive/v0.4.0.tar.gz"
-  sha256 "7a4cb524815f9759a363f19f28cd8e496e9c6c5dedc67696e52e6ecefc37af63"
+  version "0.4.1"
+  url "https://github.com/measurement-kit/cross/archive/v0.4.1.tar.gz"
+  sha256 "1b026beb7ec896fac5de6b8da5072cde507aca944cfd6a64323cdcc9ad3f6014"
 
   depends_on "autoconf" => :build
   depends_on "automake" => :build
