16c5f335adbf5b7ede6ed307622050df75a97d53
Simone Basso <bassosimone@gmail.com>
2019-06-04 19:12:19 +0200

    android-measurement-kit: statically link binary
    Necessary to run smoke tests on devices.

--- android-measurement-kit.rb
+++ android-measurement-kit.rb
@@ -4,11 +4,11 @@ class AndroidMeasurementKit < Formula
   version "0.10.4"
   url "https://github.com/measurement-kit/measurement-kit/archive/v0.10.4.tar.gz"
   sha256 "6ca0d9e7a9c1ff0ea8713bf59fde9f87365acdc4b784a5a4bb3f35a77bc4b775"
-  revision 1
+  revision 2
 
   bottle do
     root_url "https://dl.bintray.com/measurement-kit/homebrew"
-    sha256 "c83857dddfefba232c90ea02be7a92b5f160488984269d53881ef45a962c468e" => :mojave
+    sha256 "4cb2811c29a2358c4ab289df5d631d4b9ec7cff21850bb9eae449b4affbb9c09" => :mojave
   end
 
   depends_on "android-libevent"
@@ -26,6 +26,9 @@ class AndroidMeasurementKit < Formula
   def install
     ENV['ANDROID_NDK_ROOT'] = '/usr/local/share/android-sdk/ndk-bundle'
     system "./autogen.sh", "-n"
+    # We want to smoke test the MK executable on device. So, let's force
+    # static linking so we don't need to copy over libc++_shared.so.
+    ENV['LDFLAGS'] = "-static-libstdc++"
     [ "x86", "x86_64", "armeabi-v7a", "arm64-v8a" ].each do |arch|
       mkdir "build-#{arch}" do
         system "cross-android", "#{arch}", "../configure", "--prefix=#{prefix}/#{arch}",
