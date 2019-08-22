16401e951a084c5639e69eba695ae52a8fac7d7f
Simone Basso <bassosimone@gmail.com>
2019-08-22 12:53:47 +0200

    ios-libevent.rb: upgrade to v2.1.11

--- ios-libevent.rb
+++ ios-libevent.rb
@@ -1,12 +1,12 @@
 class IosLibevent < Formula
   desc "Asynchronous event library"
   homepage "https://libevent.org/"
-  url "https://github.com/libevent/libevent/archive/release-2.1.10-stable.tar.gz"
-  sha256 "52c9db0bc5b148f146192aa517db0762b2a5b3060ccc63b2c470982ec72b9a79"
+  url "https://github.com/libevent/libevent/archive/release-2.1.11-stable.tar.gz"
+  sha256 "229393ab2bf0dc94694f21836846b424f3532585bac3468738b7bf752c03901e"
 
   bottle do
     root_url "https://dl.bintray.com/measurement-kit/homebrew"
-    sha256 "c8c2da0e160c49c6035fc2d75656d08f1b02858b0a6cca57bd271e27ea444b4e" => :mojave
+    sha256 "dcce610c81f0de67b9a62fc81a3c04bf39544141aae0749eec23c2ac40af9a68" => :mojave
   end
 
   depends_on "autoconf" => :build
