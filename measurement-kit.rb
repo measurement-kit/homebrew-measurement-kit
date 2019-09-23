70482004c4edea781b963b0a82034b8c503c7a5b
Simone Basso <bassosimone@gmail.com>
2019-09-23 16:44:47 +0200

    measurement-kit.rb: rebuild and link with openssl@1.1
    We were not linking with the right version of openssl and this
    was causing some annoying link errors.

--- measurement-kit.rb
+++ measurement-kit.rb
@@ -4,15 +4,17 @@ class MeasurementKit < Formula
   version "0.10.6"
   url "https://github.com/measurement-kit/measurement-kit/archive/v0.10.6.tar.gz"
   sha256 "5ec94e522c3bc43cbf749659c18d4b13bcfbb2874db4d6b4e21b160d76dd5bd0"
+  revision 1
 
   bottle do
     root_url "https://dl.bintray.com/measurement-kit/homebrew"
-    sha256 "da542423433b7eb7ae2150c9ded36dd3e624fd37b1f89e8bd568b3c1f12ef078" => :mojave
+    sha256 "6f66f6cf39ec230685723f4eb1efc309ce68703643e636acffc61de2676c53cd" => :mojave
   end
 
   depends_on "libevent"
   depends_on "libmaxminddb"
   #depends_on "curl" # Wrong! we want to depend on macOS's curl
+  depends_on "openssl@1.1"
 
   depends_on "git" => :build
   depends_on "autoconf" => :build
