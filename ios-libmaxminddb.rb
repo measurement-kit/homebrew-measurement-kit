9a2e55ceedea351528c30a37513139ccf863f8d1
Simone Basso <bassosimone@gmail.com>
2019-12-17 12:32:16 +0100

    ios: build libmaxminddb v1.4.2

--- ios-libmaxminddb.rb
+++ ios-libmaxminddb.rb
@@ -1,18 +1,17 @@
 class IosLibmaxminddb < Formula
   desc "C library for the MaxMind DB file format"
   homepage "https://github.com/maxmind/libmaxminddb"
-  url "https://github.com/maxmind/libmaxminddb/releases/download/1.3.2/libmaxminddb-1.3.2.tar.gz"
-  sha256 "e6f881aa6bd8cfa154a44d965450620df1f714c6dc9dd9971ad98f6e04f6c0f0"
-  revision 1
+  url "https://github.com/maxmind/libmaxminddb/releases/download/1.4.2/libmaxminddb-1.4.2.tar.gz"
+  sha256 "dd582aa971be23dee960ec33c67fb5fd38affba508e6f00ea75959dbd5aad156"
 
   bottle do
     root_url "https://dl.bintray.com/measurement-kit/homebrew"
-    sha256 "08b292b748c4bb70eb4a3b845e2a9fa984733f0f30c0b95bee78d6ca1d1a8742" => :mojave
+    sha256 "e9011913ae7bde1f94951dbfcb07632ca758bcbfd14b6cd6b639ac007dbca6b6" => :mojave
   end
 
   depends_on "cross" => :build
 
-  keg_only "iOS build that we should not install system wide"
+  keg_only "this is an iOS build that we should not install system wide"
 
   def install
     [ "i386", "x86_64", "armv7s", "arm64" ].each do |arch|
