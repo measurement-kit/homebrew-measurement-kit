7c186414c0c096f557e6a186b35326c11e6b74d2
Simone Basso <bassosimone@gmail.com>
2019-12-19 14:39:26 +0100

    android: build libmaxminddb v1.4.2

--- android-libmaxminddb.rb
+++ android-libmaxminddb.rb
@@ -1,17 +1,17 @@
 class AndroidLibmaxminddb < Formula
   desc "C library for the MaxMind DB file format"
   homepage "https://github.com/maxmind/libmaxminddb"
-  url "https://github.com/maxmind/libmaxminddb/releases/download/1.3.2/libmaxminddb-1.3.2.tar.gz"
-  sha256 "e6f881aa6bd8cfa154a44d965450620df1f714c6dc9dd9971ad98f6e04f6c0f0"
+  url "https://github.com/maxmind/libmaxminddb/releases/download/1.4.2/libmaxminddb-1.4.2.tar.gz"
+  sha256 "dd582aa971be23dee960ec33c67fb5fd38affba508e6f00ea75959dbd5aad156"
 
   bottle do
     root_url "https://dl.bintray.com/measurement-kit/homebrew"
-    sha256 "b1444b02f53101098a1668389b92485b0ec8a811df6f8ee0ed60db5cba307042" => :mojave
+    sha256 "3e0d465a72fc59a75236a62f35dc885262570f3522b028dd6ccb0fdcce2e7056" => :mojave
   end
 
   depends_on "cross" => :build
 
-  keg_only "Android build that we should not install system wide"
+  keg_only "this is an Android build that we should not install system wide"
 
   def install
     ENV['ANDROID_NDK_ROOT'] = '/usr/local/share/android-sdk/ndk-bundle'
