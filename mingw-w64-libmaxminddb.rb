c8fb192ab3498d9f6a1ceec6cb23d06baaf84eaf
Simone Basso <bassosimone@gmail.com>
2019-12-20 17:23:36 +0100

    mingw-w64: build libmaxminddb v1.4.2

--- mingw-w64-libmaxminddb.rb
+++ mingw-w64-libmaxminddb.rb
@@ -1,37 +1,26 @@
 class MingwW64Libmaxminddb < Formula
   desc "C library for the MaxMind DB file format"
   homepage "https://github.com/maxmind/libmaxminddb"
-  url "https://github.com/maxmind/libmaxminddb/releases/download/1.3.2/libmaxminddb-1.3.2.tar.gz"
-  sha256 "e6f881aa6bd8cfa154a44d965450620df1f714c6dc9dd9971ad98f6e04f6c0f0"
+  url "https://github.com/maxmind/libmaxminddb/releases/download/1.4.2/libmaxminddb-1.4.2.tar.gz"
+  sha256 "dd582aa971be23dee960ec33c67fb5fd38affba508e6f00ea75959dbd5aad156"
 
   bottle do
     root_url "https://dl.bintray.com/measurement-kit/homebrew"
-    sha256 "807450658cb0d2fd320c4696febd88af706bd73e214034eb2ac43f563d70b835" => :mojave
+    sha256 "728eff5562a1850d87072e2c6e024da5450b9c71915fb46ef1232cffb8ccaa02" => :mojave
   end
 
+  depends_on "cross" => :build
   depends_on "mingw-w64" => :build
 
-  keg_only "Windows build that we should not install system wide"
+  keg_only "this is a Windows build that we should not install system wide"
 
   def install
-
-    ENV['AR'] = 'x86_64-w64-mingw32-ar'
-    ENV['AS'] = 'x86_64-w64-mingw32-as'
-    ENV['CC'] = 'x86_64-w64-mingw32-gcc'
-    ENV['CFLAGS'] = '-Wall -O2'
-    ENV['CPP'] = 'x86_64-w64-mingw32-cpp'
-    ENV['CXX'] = 'x86_64-w64-mingw32-g++'
-    ENV['CXXFLAGS'] = '-Wall -O2'
-    ENV['LD'] = 'x86_64-w64-mingw32-ld'
-    ENV['NM'] = 'x86_64-w64-mingw32-nm'
-    ENV['RANLIB'] = 'x86_64-w64-mingw32-ranlib'
-    ENV['STRIP'] = 'x86_64-w64-mingw32-strip'
-
-    system "./configure", "--disable-debug",
-                          "--host=x86_64-w64-mingw32",
+    ENV['PATH'] = '/usr/local/bin:/usr/bin:/bin'
+    system "cross-mingw", "x86_64", "./configure", "--disable-debug",
                           "--disable-dependency-tracking",
                           "--disable-shared",
                           "--disable-tests",
+                          "--disable-binaries",
                           "--prefix=#{prefix}"
     system "make", "install"
   end
