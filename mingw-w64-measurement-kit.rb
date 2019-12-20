57ac81676b739a3dcd91a737adf2510af05fa960
Simone Basso <bassosimone@gmail.com>
2019-12-20 18:23:38 +0100

    mingw-w64: build measurement-kit v0.10.7

--- mingw-w64-measurement-kit.rb
+++ mingw-w64-measurement-kit.rb
@@ -1,42 +1,32 @@
 class MingwW64MeasurementKit < Formula
   desc "Network measurement engine"
   homepage "https://measurement-kit.github.io/"
-  version "0.10.6"
-  url "https://github.com/measurement-kit/measurement-kit/archive/v0.10.6.tar.gz"
-  sha256 "5ec94e522c3bc43cbf749659c18d4b13bcfbb2874db4d6b4e21b160d76dd5bd0"
+  version "0.10.7"
+  url "https://github.com/measurement-kit/measurement-kit/archive/v0.10.7.tar.gz"
+  sha256 "abe8f2f24ed64c0fed9b439cb7f335456f11ac204a8d679c31595cdec87c6b7b"
 
   bottle do
     root_url "https://dl.bintray.com/measurement-kit/homebrew"
-    sha256 "a2cd90aa9a92818994f122b43e10cd97967df78959969375bd38493b700247c0" => :mojave
+    sha256 "0a6106b00ba76a49fd0801a2885263089eb7558b3263746429d8727235b55f62" => :mojave
   end
 
   depends_on "mingw-w64-libevent"
   depends_on "mingw-w64-libmaxminddb"
   depends_on "mingw-w64-curl"
 
-  depends_on "git" => :build
+  depends_on "cross" => :build
   depends_on "autoconf" => :build
   depends_on "automake" => :build
+  depends_on "git" => :build
   depends_on "libtool" => :build
 
-  keg_only "Windows build that we should not install system wide"
+  keg_only "this is a Windows build that we should not install system wide"
 
   def install
 
-    ENV['AR'] = 'x86_64-w64-mingw32-ar'
-    ENV['AS'] = 'x86_64-w64-mingw32-as'
-    ENV['CC'] = 'x86_64-w64-mingw32-gcc'
-    ENV['CPPFLAGS'] = '-Wall -O2 -Wno-cpp'
-    ENV['CFLAGS'] = '-Wall -O2'
-    ENV['CPP'] = 'x86_64-w64-mingw32-cpp'
-    ENV['CXX'] = 'x86_64-w64-mingw32-g++'
-    ENV['CXXFLAGS'] = '-Wall -O2'
+    ENV['CPPFLAGS'] = '-Wno-cpp'
     ENV['LDFLAGS'] = '-static'
-    ENV['LD'] = 'x86_64-w64-mingw32-ld'
-    ENV['NM'] = 'x86_64-w64-mingw32-nm'
     ENV['PATH'] = '/usr/local/bin:/usr/bin:/bin'
-    ENV['RANLIB'] = 'x86_64-w64-mingw32-ranlib'
-    ENV['STRIP'] = 'x86_64-w64-mingw32-strip'
 
     # TODO(bassosimone): figure out why the measurement_kit binary built
     # using this procedure isn't actually static. For reference, if we
@@ -44,8 +34,7 @@ class MingwW64MeasurementKit < Formula
     # compiled here, everything works and is actually static.
 
     system "./autogen.sh", "-n"
-    system "./configure", "--prefix=#{prefix}",
-                          "--host=x86_64-w64-mingw32",
+    system "cross-mingw", "x86_64", "./configure", "--prefix=#{prefix}",
                           "--disable-shared",
                           "--disable-dependency-tracking",
                           "--with-libevent=/usr/local/opt/mingw-w64-libevent",
