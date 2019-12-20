fbcc4cf1cc5106739af192bc57f6c2c4edd7621d
Simone Basso <bassosimone@gmail.com>
2019-12-20 14:46:33 +0100

    cross: update to v0.4.4
    This release adds a cross build script for mingw.

--- cross.rb
+++ cross.rb
@@ -1,9 +1,9 @@
 class Cross < Formula
   desc "Cross build scripts for MK"
   homepage "https://measurement-kit.github.io/"
-  version "0.4.3"
-  url "https://github.com/measurement-kit/cross/archive/v0.4.3.tar.gz"
-  sha256 "19269f919c5b0d28985750bf5d4ee34b52f4db2d84ae13cbb33047c76f170957"
+  version "0.4.4"
+  url "https://github.com/measurement-kit/cross/archive/v0.4.4.tar.gz"
+  sha256 "d775612ece32584a35175584d3c2111cbba639c44d64822520a44d03f537f5a5"
 
   depends_on "autoconf" => :build
   depends_on "automake" => :build
