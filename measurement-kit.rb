e8e51ec13ccf3a869a924ba8207daf4515a156d0
Simone Basso <bassosimone@gmail.com>
2019-12-23 10:54:01 +0100

    macos: build measurement-kit v0.10.8

--- measurement-kit.rb
+++ measurement-kit.rb
@@ -1,13 +1,13 @@
 class MeasurementKit < Formula
   desc "Network measurement engine"
   homepage "https://measurement-kit.github.io/"
-  version "0.10.7"
-  url "https://github.com/measurement-kit/measurement-kit/archive/v0.10.7.tar.gz"
-  sha256 "abe8f2f24ed64c0fed9b439cb7f335456f11ac204a8d679c31595cdec87c6b7b"
+  version "0.10.8"
+  url "https://github.com/measurement-kit/measurement-kit/archive/v0.10.8.tar.gz"
+  sha256 "773c0f03fa21b56b34a24f97b4fedf96e66b0f49ef549d5c674e63ed43e1c278"
 
   bottle do
     root_url "https://dl.bintray.com/measurement-kit/homebrew"
-    sha256 "3f3591581fccabaa40e935de3d1a27da35654e89ef23b53cf424627f10d4f92f" => :mojave
+    sha256 "bf1868173b9717f8f284bb791288299931d516e44838d5eeb7b31a5dc7b192b2" => :mojave
   end
 
   depends_on "libevent"
