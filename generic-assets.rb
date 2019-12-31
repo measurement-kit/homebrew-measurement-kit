0e45c6138392d8a5e0b3fbd42686dc40bcf0b72f
Simone Basso <bassosimone@gmail.com>
2019-12-31 13:05:00 +0100

    generic-assets: upgrade to 20191226162429
    Closes #27

--- generic-assets.rb
+++ generic-assets.rb
@@ -1,16 +1,19 @@
 class GenericAssets < Formula
   desc "Generic assets required to do Measurement Kit development"
   homepage "https://measurement-kit.github.io/"
-  version "20190822135402"
+  version "20191226162429"
   url "https://github.com/measurement-kit/generic-assets/archive/#{version}.tar.gz"
-  sha256 "95d3959659d2904d9667a32cd30abbb4ec071472f2da654fdb2895843085c9d8"
+  sha256 "62af9a0c9b22e4b9ded16f00e74774b6b916d0543fd3969bdb3259ec89286b8a"
   keg_only "we don't want to install this globally"
   def install
     repoURL = "https://github.com/measurement-kit/generic-assets"
-    tarball = "generic-assets-#{version}.tar.gz"
-    system "curl", "-fsSLo", "#{tarball}",
-      "#{repoURL}/releases/download/#{version}/#{tarball}"
-    system "install", "-d", "#{prefix}"
-    system "tar", "-C", "#{prefix}", "-xzf", "#{tarball}"
+    for name in ["asn.mmdb", "ca-bundle.pem", "country.mmdb"] do
+      gzball = "#{name}.gz"
+      system "curl", "-fsSLo", "#{gzball}",
+        "#{repoURL}/releases/download/#{version}/#{gzball}"
+      system "install", "-d", "#{prefix}"
+      system "gunzip", "#{gzball}"
+      system "mv", "#{name}", "#{prefix}"
+    end
   end
 end
