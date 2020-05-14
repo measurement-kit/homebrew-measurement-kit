7740b3664455273b7cbd404a140e711c78c2496c
Simone Basso <bassosimone@gmail.com>
2020-05-14 20:17:38 +0200

    ios-libmaxminddb.rb: recompile

--- ios-libmaxminddb.rb
+++ ios-libmaxminddb.rb
@@ -3,11 +3,12 @@ class IosLibmaxminddb < Formula
   homepage "https://github.com/maxmind/libmaxminddb"
   url "https://github.com/maxmind/libmaxminddb/releases/download/1.4.2/libmaxminddb-1.4.2.tar.gz"
   sha256 "dd582aa971be23dee960ec33c67fb5fd38affba508e6f00ea75959dbd5aad156"
+  revision 1
 
   bottle do
     root_url "https://dl.bintray.com/measurement-kit/homebrew"
-    sha256 "e9011913ae7bde1f94951dbfcb07632ca758bcbfd14b6cd6b639ac007dbca6b6" => :mojave
-    sha256 "601e2e465aefeff8e86b6a7f826f0041ee2dad9d24495a163832272de83514a6" => :catalina
+    cellar :any_skip_relocation
+    sha256 "0ea8e501e3581e10c74bd0da6e47dbf9ba9075e0e8748133df73150d7f56603c" => :catalina
   end
 
   depends_on "cross" => :build
@@ -15,6 +16,7 @@ class IosLibmaxminddb < Formula
   keg_only "this is an iOS build that we should not install system wide"
 
   def install
+    ENV['PATH'] = '/usr/local/bin:/usr/bin:/bin'
     [ "i386", "x86_64", "armv7s", "arm64" ].each do |arch|
       mkdir "build-#{arch}" do
         system "cross-ios", "#{arch}", "../configure", "--disable-debug",
