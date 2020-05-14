789badd5fc75461e5bb68c34aec9f543d3885479
Simone Basso <bassosimone@gmail.com>
2020-05-14 19:54:30 +0200

    ios-libevent.rb: recompile

--- ios-libevent.rb
+++ ios-libevent.rb
@@ -3,12 +3,12 @@ class IosLibevent < Formula
   homepage "https://libevent.org/"
   url "https://github.com/libevent/libevent/archive/release-2.1.11-stable.tar.gz"
   sha256 "229393ab2bf0dc94694f21836846b424f3532585bac3468738b7bf752c03901e"
-  revision 1
+  revision 2
 
   bottle do
     root_url "https://dl.bintray.com/measurement-kit/homebrew"
-    sha256 "be69f25e253446a4b910565f863339a71c4290abe42dfe84614c952e4ddfa26e" => :mojave
-    sha256 "baa073fd4fbdc1f0e494130bb9f574d8482fd30028c775bdf281725961c1e3c8" => :catalina
+    cellar :any_skip_relocation
+    sha256 "7257b420305fda80b70c02f486c63a3387c3cf350f02a70ed365ab05548f13ff" => :catalina
   end
 
   depends_on "autoconf" => :build
@@ -21,6 +21,7 @@ class IosLibevent < Formula
   keg_only "this is an iOS build that we should not install system wide"
 
   def install
+    ENV['PATH'] = '/usr/local/bin:/usr/bin:/bin'
     system "./autogen.sh"
     [ "i386", "x86_64", "armv7s", "arm64" ].each do |arch|
       mkdir "build-#{arch}" do
