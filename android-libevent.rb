63b10eca16884df8e53982d8aadb8f0e909ff782
Simone Basso <bassosimone@gmail.com>
2020-07-16 17:41:17 +0200

    android-libevent: compile v2.1.12

--- android-libevent.rb
+++ android-libevent.rb
@@ -1,14 +1,13 @@
 class AndroidLibevent < Formula
   desc "Asynchronous event library"
   homepage "https://libevent.org/"
-  url "https://github.com/libevent/libevent/archive/release-2.1.11-stable.tar.gz"
-  sha256 "229393ab2bf0dc94694f21836846b424f3532585bac3468738b7bf752c03901e"
-  revision 3
+  url "https://github.com/libevent/libevent/archive/release-2.1.12-stable.tar.gz"
+  sha256 "7180a979aaa7000e1264da484f712d403fcf7679b1e9212c4e3d09f5c93efc24"
 
   bottle do
     root_url "https://dl.bintray.com/measurement-kit/homebrew"
     cellar :any_skip_relocation
-    sha256 "68161b7ecd94d309718353925fde476ebbbe21c13ef83333fc334e742ebe53f3" => :catalina
+    sha256 "83355fc8b963d943015527fc8cba598a1a8784c1850967ac5460c2f1b6ae5c98" => :catalina
   end
 
   depends_on "android-libressl"
