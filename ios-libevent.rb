3303ed2da5af15dba54d3e3e18c4ee7504df871e
Simone Basso <bassosimone@gmail.com>
2020-07-17 13:10:12 +0200

    Build v2.1.12

--- ios-libevent.rb
+++ ios-libevent.rb
@@ -1,22 +1,21 @@
 class IosLibevent < Formula
   desc "Asynchronous event library"
   homepage "https://libevent.org/"
-  url "https://github.com/libevent/libevent/archive/release-2.1.11-stable.tar.gz"
-  sha256 "229393ab2bf0dc94694f21836846b424f3532585bac3468738b7bf752c03901e"
-  revision 2
+  url "https://github.com/libevent/libevent/archive/release-2.1.12-stable.tar.gz"
+  sha256 "7180a979aaa7000e1264da484f712d403fcf7679b1e9212c4e3d09f5c93efc24"
 
   bottle do
     root_url "https://dl.bintray.com/measurement-kit/homebrew"
     cellar :any_skip_relocation
-    sha256 "7257b420305fda80b70c02f486c63a3387c3cf350f02a70ed365ab05548f13ff" => :catalina
+    sha256 "c196301332bab839423a5d29e3e61f598d84091bcb2b67ad88b84fba8e2417c0" => :catalina
   end
 
+  depends_on "ios-libressl"
   depends_on "autoconf" => :build
   depends_on "automake" => :build
+  depends_on "cross" => :build
   depends_on "libtool" => :build
   depends_on "pkg-config" => :build
-  depends_on "cross" => :build
-  depends_on "ios-libressl"
 
   keg_only "this is an iOS build that we should not install system wide"
 
