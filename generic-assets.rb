0c9c32f8ad6844f02986ef321abab72df1896dd5
Simone Basso <bassosimone@gmail.com>
2020-07-21 12:24:30 +0200

    generic-assets.rb: upgrade to 20200721121920

--- generic-assets.rb
+++ generic-assets.rb
@@ -1,10 +1,10 @@
 class GenericAssets < Formula
   desc "Generic assets required to do Measurement Kit development"
   homepage "https://measurement-kit.github.io/"
-  version "20200619115947"
+  version "20200721121920"
 
   url "https://github.com/measurement-kit/generic-assets/archive/#{version}.tar.gz"
-  sha256 "93d4de4d4b46e5b935ef007015370d576476ff6b5088c31571c9dafbf28ef8cc"
+  sha256 "e9599cb91cdf908edf55b56b3a53c9c64e5bfab8c00b0bb195a8e765559fee52"
 
   keg_only "we don't want to install this globally"
   def install
